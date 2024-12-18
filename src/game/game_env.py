d130886fb03268dff120e0e62806fb497fa49176
seb-thoms <sebthomas.4199@gmail.com>
2024-12-17 17:18:57 -0800

    defined step function

--- src/game/game_env.py
+++ src/game/game_env.py
@@ -5,8 +5,16 @@ import numpy as np
 class DragonPitGame(gym.Env):
     """Main game environment for the dragon pit game."""
     
-    def __init__(self):
+    def __init__(self, screen_height: int):
+        """
+        Constructor for the game environment.
+
+        Args:
+            screen_height (int): Height of the GUI display
+        """
         super(DragonPitGame, self).__init__()
+
+        self.height = screen_height
 
         # Define action space here.
         # 3 actions for each dragon.
@@ -17,15 +25,73 @@ class DragonPitGame(gym.Env):
 
         # Define observation space here.
         self.observation_space = spaces.Box(low=np.array(0, 0, 0, 0),
-                                            high=np.array(1, 1, 100, 100),
+                                            high=np.array(self.height, self.height, 100, 100),
                                             dtype=np.int32)
+        
+        # Initial positions and health.
+        self.dragon1 = None
+        self.dragon2 = None
+        self.dragon1_health = 100
+        self.dragon2_health = 100
+
+        # Controls how much a dragon will move.
+        self.step_size = self.height // 10
+        
+        self.done = False
+        self.state = None
     
 
     def reset(self):
-        pass
+        """
+        Resets the environment.
+        """
+        self.done = False
+        self.state = [0, 0, 100, 100]
 
-    def step(self, action):
-        pass
+    def step(self, action: tuple) -> tuple[list, tuple[float, float], bool, dict]:
+        """
+        Executes one step in the environment and returns the new observation, reward, done and info.
 
-    def render(self):
+        Args:
+            action (tuple): A tuple containing actions of each dragon. 
+            eg: (0, 2) means dragon 1 moves down(0) and dragon 2 spits fire(2). 
+
+        Returns:
+            observation (list)  :   New observation from the environment.
+            reward  (tuple)     :   Reward for the action. Both dragon has its own rewards.
+            done    (bool)      :   Whether game has ended.
+            info    (dict)      :   Debugging info (as mandated by Gym).
+        """
+
+        dragon1_action, dragon2_action = action
+        dragon1_reward, dragon2_reward = 0.0, 0.0
+
+        # Initialize rewards
+
+
+
+        # Track dragon 1 action and update.
+
+
+        # Track dragon 2 action and update.
+
+        
+        # Spit fire logic. If hit, decrease health by 10. Give reward for succesful hit and penalize for getting hit.
+
+
+        # Logic for game has ended or not. 100 for winning. -100 for losing as rewards.
+
+
+        # Constrcut new observation
+        observation = [self.dragon1, self.dragon2, self.dragon1_health, self.dragon2_health]
+
+        return observation, (dragon1_reward, dragon2_reward), self.done, {}
+
+    def render(self, mode='human'):
+        """
+        Rendering logic.
+
+        Args:
+            mode (str, optional): Rendering mode. Defaults to 'human'.
+        """
         pass
